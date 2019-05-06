97012a0c833c0de5f6372fd5a5eebfda12fdaa30
rankyangel2014 <rankyangel2014@users.noreply.github.com>
2019-05-06 10:08:15 +0800

    Update first.py

--- application/views/first.py
+++ application/views/first.py
@@ -1,1 +1,22 @@
+from flask import Blueprint, jsonify
+from application.models.company import Company, CompanySchema
 
+
+first = Blueprint('first', __name__, url_prefix='/first')
+
+company_schema = CompanySchema()
+companys_schema = CompanySchema(many=True)
+
+
+@first.route('/')
+def getCompanyList():
+    list = Company.query.all()
+    result = companys_schema.dump(list)
+    return jsonify(result)
+
+
+@first.route('/<int:id>')
+def getCompanyById(id):
+    company = Company.query.get(id)
+    result = company_schema.dump(company)
+    return jsonify(result)
